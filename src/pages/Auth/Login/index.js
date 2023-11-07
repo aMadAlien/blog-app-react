ee0a1af569bf6ba15107c329d308294f11f4a318
iren <irasibil2@gmail.com>
2023-11-07 14:10:26 +0200

    feature: created request for login

--- src/pages/Auth/Login/index.js
+++ src/pages/Auth/Login/index.js
@@ -1,21 +1,33 @@
+import axiosInstance from "../../../config/axios";
 
 const Login = () => {
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosInstance.post('/auth/login', {
+      username: e.target.username.value,
+      password: e.target.password.value
+    })
+    .then(res => localStorage.setItem('access_token', res.data.access_token) )
+    .catch(e => console.error(e))
+  }
+
   return (
     <div className="row g-5 mt-3">
       <div className="col-md-7 col-lg-8 mx-auto">
         <h4 className="mb-3">Log in </h4>
-        <form className="needs-validation">
+        <form onSubmit={handleSubmit} className="needs-validation">
           <div className="row g-3">
             <div className="col-12">
-              <label for="email" className="form-label">Username or Email</label>
-              <input type="text" className="form-control" id="username-email" placeholder="you@example.com" required />
+              <label htmlFor="username" className="form-label">Username</label>
+              <input type="text" className="form-control" id="username" placeholder="you@example.com" required />
               <div className="invalid-feedback">
-                Please enter your username or email address.
+                Please enter your username.
               </div>
             </div>
             <div className="col-12">
-              <label for="address" className="form-label">Password</label>
-              <input type="password" className="form-control" id="address" placeholder="5RjeG4fc7y" minLength="8" maxLength="24" required />
+              <label htmlFor="password" className="form-label">Password</label>
+              <input type="password" className="form-control" id="password" placeholder="5RjeG4fc7y" minLength="8" maxLength="24" required />
               <div className="invalid-feedback">
                 Please enter your password.
               </div>

0a58c7491ccca8766687329cad7326f1f6ff12e8
iren <irasibil2@gmail.com>
2023-11-07 12:46:51 +0200

    created layout for login form

--- src/pages/Auth/Login/index.js
+++ src/pages/Auth/Login/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="row g-5 mt-3">
+      <div className="col-md-7 col-lg-8 mx-auto">
+        <h4 className="mb-3">Log in </h4>
+        <form className="needs-validation">
+          <div className="row g-3">
+            <div className="col-12">
+              <label for="email" className="form-label">Username or Email</label>
+              <input type="text" className="form-control" id="username-email" placeholder="you@example.com" required />
+              <div className="invalid-feedback">
+                Please enter your username or email address.
+              </div>
+            </div>
+            <div className="col-12">
+              <label for="address" className="form-label">Password</label>
+              <input type="password" className="form-control" id="address" placeholder="5RjeG4fc7y" minLength="8" maxLength="24" required />
+              <div className="invalid-feedback">
+                Please enter your password.
+              </div>
+            </div>
+          </div>
+          <hr className="my-4" />
+          <button className="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
+        </form>
+      </div>
+    </div>
   )
 }
-
 export default Login

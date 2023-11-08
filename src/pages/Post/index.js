c94fac96196b32b47875dfcb2451842ebfd10f72
iren <irasibil2@gmail.com>
2023-11-08 10:38:28 +0200

    feature: implemented anchor for comment replies

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import moment from 'moment'
+import cn from 'classname';
 import axiosInstance from '../../config/axios'
 import CreateCommentForm from "./CreateCommentForm";
 
@@ -9,6 +10,7 @@ const Post = () => {
   const [post, setPost] = useState({});
   const [comments, setComments] = useState([]);
   const [openReplie, setOpenReplie] = useState(0);
+  const [focusedComment, setfocusedComment] = useState(0);
 
   const { id } = useParams();
 
@@ -50,10 +52,21 @@ const Post = () => {
 
       <div className="list-group my-3">
         {comments.map(comment => 
-          <div key={comment.id} className="list-group-item list-group-item-action gap-3 py-3" aria-current="true">
+          <div
+            key={comment.id}
+            id={`comment-${comment.id}`}
+            className={cn("list-group-item list-group-item-action gap-3 py-3", {"list-group-item-primary": focusedComment === comment.id})}
+            aria-current="true">
             <div className="d-flex gap-2 w-100 justify-content-between">
               <div>
-                <h6 className="mb-0">{comment.user.username}</h6>
+                <h6 className="d-inline fs-5 me-3">{comment.user.username}</h6>
+                {
+                comment.comment_id &&
+                  <a
+                    href={`#comment-${comment.comment_id}`}
+                    onClick={() => setfocusedComment(comment.comment_id)}
+                  >replie to </a>
+                }
                 <p className="mb-0 opacity-75">{comment.comment}</p>
               </div>
               <div>
